b6a7939551eb3f1b67f6d7fb121e390dee558158
Ismael Tejeda <ismaeltejeda140@gmail.com>
2024-03-21 00:21:10 -0600

    Adds a second function for testing and optimization as well as a more optimized collatz function.

--- code.js
+++ code.js
@@ -1,11 +1,13 @@
 let currentInteger = 0
 let checkedIntegers = []
+let current = 0
+let checked = []
+
 function collatz(n, sequence = []) {
 	let x = null
 
 	if (sequence.includes(n)) return { n, sequence }
 	if (n < currentInteger) return false
-	if (checkedIntegers.includes(n)) return false
 
 	sequence.push(n)
 	if (n === 1) return false
@@ -15,17 +17,36 @@ function collatz(n, sequence = []) {
 	return collatz(x, sequence)
 }
 
+function collatzV2(n, sequence = []) {
+	if (sequence.includes(n)) return { n, sequence }
+	if (n < current) return false
+
+	sequence.push(n)
+
+	if (n === 1) return false
+	return n % 2 === 1
+		? collatzV2((3 * n + 1) / 2, sequence)
+		: collatzV2(n / 2, sequence)
+}
+
 function main() {
 	let x = 2 ** 20
 
-	console.time("Total Time")
+	// console.time("Original Collatz Function")
+	// for (let index = 0; index < x; index += 1) {
+	// 	let result = collatz(index)
+	// 	if (result !== false) console.log(result)
+	// 	currentInteger += 1
+	// }
+	// console.timeEnd("Original Collatz Function")
+	console.time("Collatz V2")
 	for (let index = 0; index < x; index += 1) {
-		let result = collatz(index)
+		let result = collatzV2(index)
 		if (result !== false) console.log(result)
-		currentInteger += 1
+		current += 1
 	}
+	console.timeEnd("Collatz V2")
 	console.log(`Done! Checked all numbers up to ${x.toLocaleString()}!`)
-	console.timeEnd("Total Time")
 }
 
 main()
